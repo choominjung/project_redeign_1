d89ceec4cef47abc5a6f1f3a35917decdb184415
choominjung <lovegojy@naver.com>
2022-02-07 01:40:59 +0900

    추가작업

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $('.slick-box').slick({
+
+        autoplay : true ,
+        autoplaySpeed : 3000 ,
+        arrows : true ,
+        dots : true ,
+        rows : 1 ,
+        slidesToShow : 4 ,
+    })
+
+
+});
